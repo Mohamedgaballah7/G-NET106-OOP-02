abc5e2f0748c7a3fefc23890490f4d0f746840e0
Mohamedgaballah7 <modys1106@gmail.com>
2026-09-15 17:17:53 +0300

    ans quwstion one all

--- c#oop02/Program.cs
+++ c#oop02/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //QUESTION 1:
+            // a-What is the difference between a class and a struct?
+            /*
+             A class is a reference type, while a struct is a value type, and constructor in struct while intializing the 
+            constructor they remove the default constructor but in class we can have default constructor and parameterized constructor.
+             */
+            //b-Why are classes more suitable than structs for large applications?
+            /*
+             Classes are more suitable than structs for large applications because they support inheritance, polymorphism, and encapsulation,
+            also struct store data in stack memory and class store data in heap memory, so class is more suitable for large applications.
+             */
+
         }
     }
 }
